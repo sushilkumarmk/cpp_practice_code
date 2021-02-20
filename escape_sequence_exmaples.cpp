7b76b47a737b55c721c3275c16b9aa0abe2080c1
Sushil K <61368283+sushilkumarmk@users.noreply.github.com>
2021-02-20 11:34:59 -0700

    Add files via upload

---
+++ escape_sequence_exmaples.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello \t World \n this is an example\a of \'escape sequences\'\a\a\n";
+    return 0;
+}
---
+++ hash_define.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#define LENGTH 10   
+#define WIDTH  5
+
+int main()
+{
+    const int area = LENGTH*WIDTH;
+    std::cout<<area<<std::endl;
+    return 0;
+}
