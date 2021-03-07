f0f5a9f1f1ed6a40717bf0f82ecd7562bcd8836c
Sushil K <61368283+sushilkumarmk@users.noreply.github.com>
2021-03-06 17:42:36 -0700

    Create 9_Functions.cpp

---
+++ 9_Functions.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int function_arg_pass_by_value(int a, int b);
+int function_arg_pass_by_pointer(int *a, int *b);
+int function_arg_pass_by_reference(int &a, int &b);
+
+int main()
+{
+    int a=10,b=20;
+
+    cout<<"The values of a and b before, 'calling by value' are:"<<a<<"\t"<<b<<endl;
+    function_arg_pass_by_value(a,b);
+    cout<<"-----------------------------------------------------"<<endl;
+    cout<<"The values of a and b before, 'calling by pointer' are:"<<a<<"\t"<<b<<endl;
+    function_arg_pass_by_pointer(&a,&b);
+    cout<<"-----------------------------------------------------"<<endl;
+    cout<<"The values of a and b before, 'calling by reference' are:"<<a<<"\t"<<b<<endl;
+    function_arg_pass_by_reference(a,b);
+    cout<<"-----------------------------------------------------"<<endl;
+    return 0;
+}
