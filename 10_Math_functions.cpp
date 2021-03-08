8c8d6be0de591d4455403a1d3fbf9697cc7db980
Sushil K <61368283+sushilkumarmk@users.noreply.github.com>
2021-03-07 20:45:32 -0700

    Create 10_Math_functions.cpp

---
+++ 10_Math_functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int a=10,b=2;
+    cout<<"Sine function:"<<sin(a)<<endl;
+    cout<<"Cosine function:"<<cos(a)<<endl;
+    cout<<"Tangent function:"<<tan(a)<<endl;
+    cout<<"Cotangent function:"<<1/tan(a)<<endl;
+    cout<<"Power function:"<<pow(a,b)<<endl;
+    cout<<"Square root function:"<<sqrt(a)<<endl;
+    return 0;
+}
